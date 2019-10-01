cf56fd078cb02cad0f53b7b05ec563d8cd56921e
Roshni Sahoo <roshni714@gmail.com>
2019-09-30 23:52:59 -0400

    updated configs

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#python train.py --config configs/mv_gaussian.yaml
-#python train.py --config configs/mv_gaussian2.yaml
-#python train.py --config configs/mv_gaussian3.yaml
+python train.py --config configs/mv_gaussian.yaml
+python train.py --config configs/mv_gaussian2.yaml
+python train.py --config configs/mv_gaussian3.yaml
 python train.py --config configs/exponential.yaml
 python train.py --config configs/exponential2.yaml
 python train.py --config configs/exponential3.yaml
